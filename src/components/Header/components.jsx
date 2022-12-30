ef5cb04e6092b11028237aa9dc2bd9b7766a3e93
Danylo Tkach <danylo.tkach@gmail.com>
2022-12-31 00:30:43 +0200

    feat: added main part heading

---
+++ src/components/Header/components.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Heading = styled.h1`
+  margin-top: 15rem;
+  font-size: 6.8rem;
+  font-family: "Anton", sans-serif;
+  text-transform: uppercase;
+
+  @media (max-width: 850px) {
+    font-size: 4.2rem;
+  }
+`;
+
+export { Heading };
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+import { Heading } from "./components";
+
+const Header = () => {
+  return (
+    <Heading>
+      never caught
+      <br />
+      off-guard again
+    </Heading>
+  );
+};
+
+export default Header;
