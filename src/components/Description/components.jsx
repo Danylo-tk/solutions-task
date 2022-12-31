6b2f54aa2025b0e593382ec93e234a18ce8f3f57
Danylo Tkach <danylo.tkach@gmail.com>
2022-12-31 13:07:41 +0200

    feat: main part description

---
+++ src/components/Description/components.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Paragraph = styled.p`
+  max-width: 56rem;
+  margin: 3rem 5rem;
+  font-size: 1.8rem;
+`;
+
+export { Paragraph };
---
+++ src/components/Description/index.jsx
@@ -0,0 +1,12 @@
+import { Paragraph } from "./components";
+
+const Description = () => {
+  return (
+    <Paragraph>
+      We are specialist in protecting your assets and reputation to make sure
+      you operate risk-free and focus on your core business.
+    </Paragraph>
+  );
+};
+
+export default Description;
