816d946c73ec59c1948d76cd4be335a8582a8b19
Danylo Tkach <danylo.tkach@gmail.com>
2023-01-07 22:29:05 +0200

    feat: added controlled inputs
    now inputs are controlled and object with their values is shown in console after submit

--- src/components/Forms/index.jsx
+++ src/components/Forms/index.jsx
@@ -2,6 +2,7 @@ import { FormsWrapper } from "./components";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
 import { SubmitButton } from "../../styles/components/SubmitButton/components";
+import { useState } from "react";
 
 const theme = createTheme({
   typography: {
@@ -11,20 +12,75 @@ const theme = createTheme({
 });
 
 const Forms = () => {
+  const [values, setValues] = useState({
+    name: "",
+    company: "",
+    phone: "",
+    email: "",
+    message: "",
+  });
+
+  const inputs = [
+    {
+      id: "1",
+      name: "name",
+      label: "Full Name",
+      variant: "standard",
+    },
+    {
+      id: "2",
+      name: "company",
+      label: "Company Name",
+      variant: "standard",
+    },
+    {
+      id: "3",
+      name: "phone",
+      label: "Phone Number",
+      variant: "standard",
+    },
+    {
+      id: "4",
+      name: "email",
+      label: "E-mail",
+      variant: "standard",
+    },
+    {
+      id: "5",
+      name: "message",
+      label: "Write Us Messages",
+      variant: "standard",
+      multiline: true,
+      rows: "5",
+    },
+  ];
+
+  const handleSubmit = (e) => {
+    console.log(values);
+    e.preventDefault();
+  };
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
   return (
     <ThemeProvider theme={theme}>
-      <FormsWrapper action="">
-        <TextField label="Full Name" variant="standard" />
-        <TextField label="Company Name" variant="standard" />
-        <TextField label="Phone number" variant="standard" />
-        <TextField label="E-mail" variant="standard" />
-        <TextField
-          label="Write Us Messages"
-          variant="standard"
-          multiline
-          rows={5}
-        />
-        <SubmitButton variant="contained" btnheight={60} btnwidth={170}>
+      <FormsWrapper action="" onSubmit={handleSubmit}>
+        {inputs.map((input) => (
+          <TextField
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <SubmitButton
+          variant="contained"
+          btnheight={60}
+          btnwidth={170}
+          type="submit"
+        >
           send message
         </SubmitButton>
       </FormsWrapper>
